9ca67220312f084c3e290201c0b4c124e52c23ca
Harshit yadav <harshityadav5499@gmail.com>
2026-01-13 00:38:43 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,29 @@ fn main() {
     // io::stdin().read_line(&mut data).unwrap() ;          
     // println!("{}: command not found" , data.trim());
 
-    loop{
-        // REPL 
+    // Repl implementation 
+    // loop{
+    //     // REPL 
+    //     print!("$ ") ;
+    //     io::stdout().flush().unwrap() ;
+
+    //     let mut data = String::new() ;
+    //     io::stdin().read_line(&mut data).unwrap() ;
+    //     let command = data.trim() ;
+    //     println!("{}: command not found" , command) ;
+    // }
+
+    // inbuilt exit implementation 
+    loop {
         print!("$ ") ;
         io::stdout().flush().unwrap() ;
 
         let mut data = String::new() ;
         io::stdin().read_line(&mut data).unwrap() ;
         let command = data.trim() ;
+
+        if command == "exit" {break} ;
         println!("{}: command not found" , command) ;
     }
+
 }
