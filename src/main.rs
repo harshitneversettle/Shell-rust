1717634a28f598a335f7e22533e0e6602d76d4c0
Harshit yadav <harshityadav5499@gmail.com>
2026-01-13 02:17:46 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -34,30 +34,57 @@ fn main() {
     // }
 
     // echo implementation 
+    // loop{
+    //     print!("$ ") ;
+    //     io::stdout().flush().unwrap() ;
 
+    //     let mut data = String::new() ;
+    //     io::stdin().read_line(&mut data).unwrap() ;
+    //     let command = data.trim() ;
+
+    //     if command == "exit" {
+    //         break ;
+    //     }
+
+    //     if command.starts_with("echo") {
+    //         let mut echo_data = String::new() ;
+    //         for i in command.split(" "){
+    //             if i == "echo" {continue} ;
+    //             echo_data.push_str(i) ;
+    //             echo_data.push_str(" ") ;
+    //         }
+    //         println!("{}" , echo_data.trim())
+    //     }else {
+    //         println!("{}: command not found" , command) ;
+    //     }
+    // }
+
+    // The type Builtin
     loop{
         print!("$ ") ;
         io::stdout().flush().unwrap() ;
 
+        let inbuilt_commands = ["echo", "exit", "type"];
         let mut data = String::new() ;
         io::stdin().read_line(&mut data).unwrap() ;
-        let command = data.trim() ;
-
-        if command == "exit" {
-            break ;
-        }
-
-        if command.starts_with("echo") {
-            let mut echo_data = String::new() ;
-            for i in command.split(" "){
-                if i == "echo" {continue} ;
-                echo_data.push_str(i) ;
-                echo_data.push_str(" ") ;
+        let command = data.trim_end() ;
+        if command == "exit" {break} ;
+        let commands_vec : Vec<&str> = command.split_whitespace().collect() ;
+        if commands_vec[0] == "type" {
+            for i in &commands_vec[1..]{
+                if inbuilt_commands.contains(i) {
+                    println!("{} is a shell builtin" , i) ;
+                }else{
+                    println!("{} not found" , i) ;
+                }
             }
-            println!("{}" , echo_data.trim())
-        }else {
-            println!("{}: command not found" , command) ;
+        }else if commands_vec[0] == "echo" {
+            for i in &commands_vec[1..]{
+                print!("{} " , i) ;
+            }
+            println!();
+        }else{
+            println!("{}: not found" , command) ;
         }
     }
-
 }
