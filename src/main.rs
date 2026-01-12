2e6ba0b8017a470f57afb71d0d28694d369ee357
Harshit yadav <harshityadav5499@gmail.com>
2026-01-13 02:27:36 +0530

    Add : inbuilt echo implementation

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,33 @@ fn main() {
     //     println!("{}: command not found" , command) ;
     // }
 
+
+
+// echo implementation 
+    // loop{
+    //     print!("$ ") ;
+    //     io::stdout().flush().unwrap() ;
+
+    //     let mut data = String::new() ;
+    //     io::stdin().read_line(&mut data).unwrap() ;
+    //     let command = data.trim() ;
+
+    //     if command == "exit" {
+    //         break ;
+    //     }
+
+    //     if command.starts_with("echo") {
+    //         let mut echo_data = String::new() ;
+    //         for i in command.split(" "){
+    //             if i == "echo" {continue} ;
+    //             echo_data.push_str(i) ;
+    //             echo_data.push_str(" ") ;
+    //         }
+    //         println!("{}" , echo_data.trim())
+    //     }else {
+    //         println!("{}: command not found" , command) ;
+    //     }
+    // }
     
 
     
