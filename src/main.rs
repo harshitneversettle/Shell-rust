2ca61e4ecc97bea48b7224b476cc9a9c1f8c4b7b
Harshit yadav <harshityadav5499@gmail.com>
2026-01-13 01:11:42 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,21 @@ fn main() {
     // }
 
     // inbuilt exit implementation 
-    loop {
+    // loop {
+    //     print!("$ ") ;
+    //     io::stdout().flush().unwrap() ;
+
+    //     let mut data = String::new() ;
+    //     io::stdin().read_line(&mut data).unwrap() ;
+    //     let command = data.trim() ;
+
+    //     if command == "exit" {break} ;
+    //     println!("{}: command not found" , command) ;
+    // }
+
+    // echo implementation 
+
+    loop{
         print!("$ ") ;
         io::stdout().flush().unwrap() ;
 
@@ -29,8 +43,21 @@ fn main() {
         io::stdin().read_line(&mut data).unwrap() ;
         let command = data.trim() ;
 
-        if command == "exit" {break} ;
-        println!("{}: command not found" , command) ;
+        if command == "exit" {
+            break ;
+        }
+
+        if command.starts_with("echo") {
+            let mut echo_data = String::new() ;
+            for i in command.split(" "){
+                if i == "echo" {continue} ;
+                echo_data.push_str(i) ;
+                echo_data.push_str(" ") ;
+            }
+            println!("{}" , echo_data.trim())
+        }else {
+            println!("{}: command not found" , command) ;
+        }
     }
 
 }
