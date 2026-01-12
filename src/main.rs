50d85accde38cc490364be514d0b727da3041bc2
Harshit yadav <harshityadav5499@gmail.com>
2026-01-13 00:24:40 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ") ;
-    io::stdout().flush().unwrap() ;
-    let mut data = String::new() ;
-    io::stdin().read_line(&mut data).unwrap() ;          
-    println!("{}: command not found" , data.trim());
+    // print!("$ ") ;
+    // io::stdout().flush().unwrap() ;
+    // let mut data = String::new() ;
+    // io::stdin().read_line(&mut data).unwrap() ;          
+    // println!("{}: command not found" , data.trim());
+
+    loop{
+        // REPL 
+        print!("$ ") ;
+        io::stdout().flush().unwrap() ;
+
+        let mut data = String::new() ;
+        io::stdin().read_line(&mut data).unwrap() ;
+        let command = data.trim() ;
+        println!("{}: command not found" , command) ;
+    }
 }
